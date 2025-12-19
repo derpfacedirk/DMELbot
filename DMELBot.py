e9e1c34cb900e98c1db8b2c1fcf48da8cc38a193
Dirk Jan Somsen <dirkjansomsen@hotmail.com>
2025-12-19 23:00:55 +0100

    Changed syncing to command structure to prevent repeated calls

--- DMELBot.py
+++ DMELBot.py
@@ -1,5 +1,5 @@
 from pydoc import describe
-
+from typing import Literal, Optional
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -27,9 +27,42 @@ client = commands.Bot(intents=intents, command_prefix="$")
 #Event gets called on startup
 @client.event
 async def on_ready():
-    synched = await client.tree.sync()
-    print(len(synched))
     print(f'We have logged in as {client.user}')
+
+
+@client.command()
+@commands.guild_only()
+@commands.is_owner()
+async def sync(ctx: commands.Context, guilds: commands.Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
+
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            ret += 1
+
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
 
 @client.tree.command(name = "whois", description="Gives the name of the player on the rank given")
 @app_commands.describe(rank = "rank to look up")
